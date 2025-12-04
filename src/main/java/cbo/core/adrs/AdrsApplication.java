85a49492ad61fab8c5078295d91b91b51a5894a2
Ephrem Daniel Asseffa <ephremd>
2025-12-04 17:03:25 +0300

    applications detail registry system init

---
+++ src/main/java/cbo/core/adrs/AdrsApplication.java
@@ -0,0 +1,13 @@
+package cbo.core.adrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cbo/core/adrs/AdrsApplicationTests.java
@@ -0,0 +1,13 @@
+package cbo.core.adrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
